3657003b24d6dae7a9c3bf17a1dee93b73cfd189
Tran Duc Quy <tranducquylhp@gmail.com>
2019-11-04 20:13:35 +0700

    ok

---
+++ index.js
@@ -0,0 +1,11 @@
+function main() {
+    var x = 5;
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    var y = 55;
+    console.log(y);
+    var y = 100;
+    console.log(y);
+}
+main();
